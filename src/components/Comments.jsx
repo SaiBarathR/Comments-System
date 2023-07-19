084af45e5b389d46296b6f212a43c07aa53b468f
Sai Barath <saibarathdeep@outlook.com>
2023-07-19 13:14:56 +0530

    added a comments component to add comments with a comment renderer

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import useTreeStructureProvider from "../customHooks/treeStructureProvider";
+import CommentsRenderer from "./CommentsRenderer";
+
 export default function Comments() {
 
+    const [commentsData, setCommentsData] = useState({ id: 1, items: [], });
+
+    const { insertNode, editNode, deleteNode } = useTreeStructureProvider();
+
+    const handleInsertNode = (folderId, item) => {
+        const finalStructure = insertNode(commentsData, folderId, item);
+        setCommentsData(finalStructure);
+    };
+
+    const handleEditNode = (folderId, value) => {
+        const finalStructure = editNode(commentsData, folderId, value);
+        setCommentsData(finalStructure);
+    };
+
+    const handleDeleteNode = (folderId) => {
+        const finalStructure = deleteNode(commentsData, folderId);
+        const temp = { ...finalStructure };
+        setCommentsData(temp);
+    };
 
     return (
         <div className="flex flex-col gap-6">
-            </div>
+            <CommentsRenderer
+                handleInsertNode={handleInsertNode}
+                handleEditNode={handleEditNode}
+                handleDeleteNode={handleDeleteNode}
+                comment={commentsData}
+            />
+        </div>
     )
 }
